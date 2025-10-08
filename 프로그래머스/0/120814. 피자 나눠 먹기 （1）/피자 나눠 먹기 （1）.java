73dde3bf36436ad1e1da2442680922b3ac5738ba
devgogogogo <imleo322@gmail.com>
2025-10-08 10:53:47 +0900

    [level 0] Title: 피자 나눠 먹기 (1), Time: 0.02 ms, Memory: 89.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/120814. 피자 나눠 먹기 （1）/피자 나눠 먹기 （1）.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int solution(int n) {
+        if (n % 7 < 1 && n % 7 >0) {
+            return n / 7 + 1;
+        } else if (n % 7 ==0) {
+            return n / 7;
+        } else if (n % 7 >= 1) {
+            return n / 7 + 1;
+        }
+        return 0;
+    }
+}
